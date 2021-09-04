92737583f09e9b4c8595333100945785304bd345
PauloGuedes2 <ph.guedes99@outlook.com>
2021-09-04 18:57:31 -0300

    Consulta com filtro (nomeCurso)

--- src/main/java/com/example/forum/repository/TopicoRepository.java
+++ src/main/java/com/example/forum/repository/TopicoRepository.java
@@ -3,5 +3,9 @@ package com.example.forum.repository;
 import com.example.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCursoNome(String nomeCurso);
 }
