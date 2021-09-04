570b90e8250dceff63ad626dea25d976f5f3c173
PauloGuedes2 <ph.guedes99@outlook.com>
2021-09-04 19:47:22 -0300

    Método Cadastrar

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,22 +1,31 @@
 package com.example.forum.controller;
 
 import com.example.forum.dto.TopicoDto;
+import com.example.forum.form.TopicoForm;
 import com.example.forum.model.Topico;
+import com.example.forum.repository.CursoRepository;
 import com.example.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.xml.ws.http.HTTPException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
-    public List<TopicoDto> lista(String nomeCurso){
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
+    public List<TopicoDto> listar(String nomeCurso) {
         if (nomeCurso == null){
             List<Topico> topicos = topicoRepository.findAll();
             return TopicoDto.converter(topicos);
@@ -25,4 +34,14 @@ public class TopicosController {
             return TopicoDto.converter(topicos);
         }
     }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
+        Topico topico = topicoForm.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
 }
---
+++ src/main/java/com/example/forum/form/TopicoForm.java
@@ -0,0 +1,37 @@
+package com.example.forum.form;
+
+import com.example.forum.model.Curso;
+import com.example.forum.model.Topico;
+import com.example.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+    private String titulo;
+    private String mensagem;
+    private String nomeCurso;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public Topico converter(CursoRepository cursoRepository) {
+        Curso curso = cursoRepository.findByNome(nomeCurso);
+        return new Topico(titulo, mensagem, curso);
+    }
+}
