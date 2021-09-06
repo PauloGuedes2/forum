f8f29f40ff114f7c2bdb8603e405bf72f8fdfc1c
PauloGuedes2 <ph.guedes99@outlook.com>
2021-09-05 22:35:38 -0300

    Método para excluir tópicos

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -39,6 +39,7 @@ public class TopicosController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriComponentsBuilder) {
         Topico topico = topicoForm.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -57,8 +58,14 @@ public class TopicosController {
     @Transactional
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Topico topico = form.atualizar (id, topicoRepository);
-
         return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
